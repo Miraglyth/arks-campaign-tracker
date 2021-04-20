14ff017cb54d4e307e38e23413a994f54c3ea255
Miraglyth <65577838+Miraglyth@users.noreply.github.com>
2021-04-20 20:18:41 +0100

    Update script.js

--- script.js
+++ script.js
@@ -14,9 +14,18 @@ function Init()
 
 function TableRefresh()
 {
+    var jsonData = require('./campaigns.json');
+    
     var tableText = "";
     tableText += '<table border="1"><tr><th>Name</th><th>Time</th></tr>';
+    
+    for (jsonItem = 0; jsonItem < jsonData.campaigns.length; jsonItem++)
+    {
+        tableText += '<tr><td>' + jsonData.campaigns[i].name + '</td><td>' + jsonData.campaigns[i].timestamp + '</td></tr>';
+    }
+    
     tableText += '<tr><td>Hello</td><td>World</td></tr>';
+    
     tableText += '</table>';
     document.getElementById("jsonTable").innerHTML = tableText;
 }
