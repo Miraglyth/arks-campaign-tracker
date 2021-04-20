2b9c799c5b868acebf67cfbebecf8ca9b7337502
Miraglyth <65577838+Miraglyth@users.noreply.github.com>
2021-04-20 13:02:09 +0100

    Update script.js

--- script.js
+++ script.js
@@ -2,11 +2,11 @@ document.addEventListener("DOMContentLoaded", function() {
     Init();
 });
 
+document.getElementById("refreshbutton").addEventListener("click", function() {
+    Init();
+});
+
 function Init()
 {
     document.getElementById("timeDisplay").innerHTML = "The time in UTC is: " + new Date(Date.now()).toUTCString();
 };
-
-document.getElementById("refreshbutton").addEventListener("click", function() {
-    Init();
-});
