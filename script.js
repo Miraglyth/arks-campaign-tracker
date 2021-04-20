2970902591bd92d2186b27c80ad4bd3fa95bf117
Miraglyth <65577838+Miraglyth@users.noreply.github.com>
2021-04-20 19:51:22 +0100

    Update script.js

--- script.js
+++ script.js
@@ -9,4 +9,14 @@ document.getElementById("refreshbutton").addEventListener("click", function() {
 function Init()
 {
     document.getElementById("timeDisplay").innerHTML = "The time in UTC is: " + new Date(Date.now()).toUTCString();
+    TableRefresh();
 };
+
+function TableRefresh()
+{
+    var tableText = "";
+    tableText += '<table border="1"><tr><th>Name</th><th>Time</th></tr>';
+    tableText += '<tr><td>Hello</td><td>World</td></tr>';
+    tableText += '</table>';
+    document.getElementById("jsonTable").innerHTML = tableText;
+}
