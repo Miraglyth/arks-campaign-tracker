e1118fb04c46dfaf8fe07b4e29aa0f6e61d65b4d
Miraglyth <miraglyth@gmail.com>
2021-04-21 01:07:46 +0100

    Test jQuery getJSON

--- script.js
+++ script.js
@@ -1,31 +1,30 @@
-document.addEventListener("DOMContentLoaded", function() {
-    Init();
+$(document).ready(function () {
+    RefreshAll();
 });
 
-document.getElementById("refreshbutton").addEventListener("click", function() {
-    Init();
+document.getElementById("refreshbutton").addEventListener("click", function () {
+    RefreshAll();
+    RefreshTable();
 });
 
-function Init()
-{
+function RefreshAll() {
     document.getElementById("timeDisplay").innerHTML = "The time in UTC is: " + new Date(Date.now()).toUTCString();
-    TableRefresh();
+    RefreshTableStatic();
 };
 
-function TableRefresh()
-{
-    var jsonData = require('./campaigns.json');
-    
+function RefreshTable() {
+    $.getJSON("campaigns.json", function (data) {
+        $.each(data, function (thing) {
+            // tableText += thing;
+            console.log(thing);
+        })
+    });
+}
+
+function RefreshTableStatic() {
     var tableText = "";
     tableText += '<table border="1"><tr><th>Name</th><th>Time</th></tr>';
-    
-    for (jsonItem = 0; jsonItem < jsonData.campaigns.length; jsonItem++)
-    {
-        tableText += '<tr><td>' + jsonData.campaigns[i].name + '</td><td>' + jsonData.campaigns[i].timestamp + '</td></tr>';
-    }
-    
     tableText += '<tr><td>Hello</td><td>World</td></tr>';
-    
     tableText += '</table>';
     document.getElementById("jsonTable").innerHTML = tableText;
 }
