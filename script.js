817fd5945c68d15a5c9fe1956eecb1a86b924c41
Miraglyth <miraglyth@gmail.com>
2021-04-21 16:27:23 +0100

    Time storage and parsing, sample campaigns

--- script.js
+++ script.js
@@ -38,26 +38,28 @@ function RefreshTable() {
 
         // Table header
         var tableText = '';
-        tableText = '<table border="1"><tr>'
-        tableText += '<th>Group Name</th>'
-        tableText += '<th>Link</th>'
-        tableText += '<th>Task Name</th>'
-        tableText += '<th>Start Time</th>'
-        tableText += '<th>End Time</th>'
-        tableText += '<th>Reward Time</th>'
-        tableText += '<th>Distribution</th>'
+        tableText = '<table border="1"><tr>';
+        tableText += '<th>Campaign Group</th>';
+        tableText += '<th>Campaign Name</th>';
+        tableText += '<th>Start Time</th>';
+        tableText += '<th>End Time</th>';
+        tableText += '<th>Reward Time</th>';
+        tableText += '<th>Distribution</th>';
+        tableText += '<th>Activity</th>';
+        tableText += '<th>Reward</th>';
         tableText += '</tr>';
 
         // Table contents
         $.each(data.campaigns, function (key, value) {
             tableText += '<tr>'
-            tableText += '<td>' + value.name_group + '</td>';
-            tableText += '<td><a href="' + value.url + '">Link</a></td>';
+            tableText += '<td><a href="' + value.url + '">' + value.name_group + '</a></td>';
             tableText += '<td>' + value.name_task + '</td>';
-            tableText += '<td>' + value.time_start + '</td>';
-            tableText += '<td>' + value.time_end + '</td>';
-            tableText += '<td>' + value.time_reward + '</td>';
+            tableText += '<td>' + DateParse(value.time_start) + '</td>';
+            tableText += '<td>' + DateParse(value.time_end) + '</td>';
+            tableText += '<td>' + DateParse(value.time_reward) + '</td>';
             tableText += '<td>' + value.distribution + '</td>';
+            tableText += '<td>' + value.task + '</td>';
+            tableText += '<td>' + value.reward + '</td>';
             tableText += '</tr>';
         });
 
@@ -67,6 +69,18 @@ function RefreshTable() {
         // Update span
         document.getElementById("jsonTable").innerHTML = tableText;
     });
+}
+
+function DateParse(string) {
+    var returnVar = "";
+    if (string != "Unknown") {
+        // returnVar = new Date(string).getDate().toString() + " " + new Date(string).getMonth().toString();
+        returnVar = new Date(string).toLocaleString();
+    }
+    else {
+        returnVar = "Unknown";
+    }
+    return returnVar;
 }
 
 function DummyCallback(data) {
